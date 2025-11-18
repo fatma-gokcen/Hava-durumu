cf73df2035336793ccb94eeec97da993700e79c0
fatmagokcen-software <fatmagokcen53@gmail.com>
2025-11-18 15:07:07 +0300

    DOM elementleri eklendi

---
+++ app.js
@@ -0,0 +1,6 @@
+// DOM elementleri eklendi
+const cityInput = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#searchBtn");
+const resultDiv = document.querySelector("#result");
+
+

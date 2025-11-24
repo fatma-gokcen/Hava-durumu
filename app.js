92ae5ad52f2066473063c1c2dbd4be0d3eb90f9b
fatmagokcen-software <fatmagokcen53@gmail.com>
2025-11-24 22:33:26 +0300

    Hava durumu verisi çekme fonksiyonu eklendi

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const resultDiv = document.querySelector("#result");
 
 //2. API Anahtarı Tanımlandı(weatherapi)
 const API_KEY = '1e107ab9424747a5a6c170558252411';
+
+//3. Hava durumu verisini çekme fonksiyonu
+async function getWeatherData(city) {
+    if (!city) {
+        resultDiv.innerHTML = "<p>Lütfen bir şehir adı giriniz</p>"
+        return;
+    }
+}
+
+

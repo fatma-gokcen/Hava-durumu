4f5623a27a947e6a2b167818f72516d4ec72e23f
fatmagokcen-software <fatmagokcen53@gmail.com>
2025-11-24 22:08:09 +0300

    Tıklama olayı iptal edildi, API anahtarı tanımlandı

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// DOM elementleri eklendi
+//1.  DOM elementleri eklendi
 const cityInput = document.querySelector("#cityInput");
 const searchBtn = document.querySelector("#searchBtn");
 const resultDiv = document.querySelector("#result");
 
-
-searchBtn.addEventListener("click", () => {
-    const city = cityInput.value;
-    console.log("Aranan şehir:", city)
-})
+//2. API Anahtarı Tanımlandı(weatherapi)
+const API_KEY = '1e107ab9424747a5a6c170558252411';

3cfe6ab97404a50243b711b24aae4489cd99a473
fatmagokcen-software <fatmagokcen53@gmail.com>
2025-11-25 20:08:32 +0300

    Hata Kontrolü eklendi

--- app.js
+++ app.js
@@ -8,13 +8,33 @@ const API_KEY = '1e107ab9424747a5a6c170558252411';
 
 //3. Hava durumu verisini çekme fonksiyonu
 async function getWeatherData(city) {
+
+    // Şehir boş mu kontrol et
     if (!city) {
-        resultDiv.innerHTML = "<p>Lütfen bir şehir adı giriniz</p>"
+        resultDiv.innerHTML = "<p>Lütfen bir şehir adı giriniz</p>";
         return;
     }
-}
-//4.Yükleniyor mesajı göster
-resultDiv.innerHTML = "<p>Hava durumu bilgisi yükleniyor...</p>"
 
-//5. Dinamik API URL oluştuurldu (key ve query paremetreleri dinamik olarak eklenir)
-const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+    //4. Yükleniyor mesajı göster
+    resultDiv.innerHTML = "<p>Hava durumu bilgisi yükleniyor...</p>";
+
+    //5. Dinamik API URL oluşturuldu 
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+
+    //6. Hata kontrolü 
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        // API bir hata gönderirse
+        if (data.error) {
+            throw new Error(data.error.message);
+        }
+
+        // Başarılıysa veriyi ekrana yazdır
+        displayWeather(data);
+
+    } catch (error) {
+        resultDiv.innerHTML = `Hata: ${error.message}`;
+    }
+}
